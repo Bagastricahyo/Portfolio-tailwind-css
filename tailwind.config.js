063d2fd7e57a3fb312f74b5534ba47896ca227fb
Bagastricahyo <bagastricahyo361@gmail.com>
2022-07-15 23:57:01 +0700

    portfolio tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+      container: {
+        center: true,
+        padding: '16px',
+      },
+    extend: {
+      colors: {
+        primary: 'rgb(20 184 166)',
+        dark: 'rgb(15 23 42)',
+        secondary: 'rgb(100 116 139)'
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
